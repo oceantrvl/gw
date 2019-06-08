5449a57aa3751877f75c6b87bc14b5e24bf9a164
oceantrvl <51275248+oceantrvl@users.noreply.github.com>
2019-06-08 08:44:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//Masukkan PHPSESSID
+$PHPSESSID = 'qppic91svq0pd8bs7h3eqflel7';
+//Masukkan GA
+$_ga = 'GA1.2.1844090742.1559956576';
+//Masukkan GID
+$_gid = 'GA1.2.1694553994.1559956576';
+
+
+
+
+
+
+
+
+
+
+?>
